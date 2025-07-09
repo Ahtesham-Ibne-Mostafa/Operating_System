e8f443919dc6e18e85c93f0fa65c73ae7f779ebe
Ahtesham Ibne Mostafa <ahtesham.ibne.mostafa@g.bracu.ac.bd>
2025-07-09 09:16:04 +0600

    Add files via upload

---
+++ Hello.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+    //printf("Hello World!\n");
+    int x=10;
+    float b=5.666464654;
+    double c=2.3333333;
+    char d="x";
+
+
+    return 0;
+}
